e0005580015fc860d454cdc5f9a3b0ea87ac6bfb
Oleksandr-D <dolbnja9@gmail.com>
2021-07-06 16:00:47 +0300

    js comit test

--- script.js
+++ script.js
@@ -18,5 +18,4 @@ document.querySelector('.square').addEventListener('mouseout', function () {
         elementIdx = 0;
     }
 });
-//чому event  заркреслений(Застарілий?) 
-//що використовувати замість  event?           
+//чому event  заркреслений(Застарілий?)що використовувати замість  event?           
