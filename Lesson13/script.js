f4919d5a4e1fb6809056b1eaceb38532a5a6878e
Oleksandr <84642054+Oleksandr-D@users.noreply.github.com>
2021-08-14 21:07:58 +0300

    updated lesson 13

--- Lesson13/script.js
+++ Lesson13/script.js
@@ -1,17 +1,4 @@
-// Треба провалідувати поля у цій формі на вхідні дані при кліку на кнопку як на відео
-//  validateRegisterForm. 
-// — Ім’я та прізвище: може бути слово англійською з великої або маленької букви
-//  і не більше 20. Цифр і інші символи не допускаються.
-// — Емейл: обов'язково @. Усі букви повинні бути англійською. Загальні вимоги
-//  наступні(будь-яка кількість букв, цифр,
-//      тире і крапок@будьяка кількість букв.( net.ua, org.ua, gmail.com. і т.д.)).
-// — Пароль: Від 8 до 15 символів можуть бути букви та цифри.
-// — Чекбокс: якщо всі поля правильно заповнені, то при кліку на чекбокс кнопка
-//  Sign Up розблоковується
-// — Sign Up: при кліку на дану кнопку відкривається модальне вікно з повідомленням
-//  про успішну реєстрацію
-// — Start exploring: при кліку на дану кнопку закривається модальне вікно,
-//  а також всі поля форми зачищуються
+// Треба провалідувати поля у цій формі на вхідні дані при кліку на кнопку...
 
 let firstNameRegExp = /^[a-zA-Z]{2,20}$/;
 let lastNameRegExp = /^[a-zA-Z]{2,20}$/;
@@ -22,76 +9,52 @@ let getID = id => document.getElementById(id);
 getID('firstName').oninput = function () {
     let testName = firstNameRegExp.test(getID('firstName').value);
     if (testName) {
-        getID('firstName').style.boxShadow = '0 0 5px 5px green';
+        this.style.boxShadow = '0 0 5px 5px green';
     } else {
-        getID('firstName').style.boxShadow = '0 0 5px 5px red';
+        this.style.boxShadow = '0 0 5px 5px red';
     }
 }
 getID('lastName').oninput = function () {
     let testLastN = lastNameRegExp.test(getID('lastName').value);
     if (testLastN) {
-        getID('lastName').style.boxShadow = '0 0 5px 5px green';
+        this.style.boxShadow = '0 0 5px 5px green';
     } else {
-        getID('lastName').style.boxShadow = '0 0 5px 5px red';
+        this.style.boxShadow = '0 0 5px 5px red';
     }
 }
 getID('email').oninput = function () {
     let testEmail = emailRegExp.test(getID('email').value);
     if (testEmail) {
-        getID('email').style.boxShadow = '0 0 5px 5px green';
+        this.style.boxShadow = '0 0 5px 5px green';
     } else {
-        getID('email').style.boxShadow = '0 0 5px 5px red';
+        this.style.boxShadow = '0 0 5px 5px red';
     }
 }
 getID('password').oninput = function () {
     let testPass = passRegExp.test(getID('password').value);
     if (testPass) {
-        getID('password').style.boxShadow = '0 0 5px 5px green';
+        this.style.boxShadow = '0 0 5px 5px green';
     } else {
-        getID('password').style.boxShadow = '0 0 5px 5px red';
+        this.style.boxShadow = '0 0 5px 5px red';
     }
 }
-//button activation on check
-getID('check').onclick = function () {
-    let testName = firstNameRegExp.test(getID('firstName').value);
-    let testLastN = lastNameRegExp.test(getID('lastName').value);
-    let testEmail = emailRegExp.test(getID('email').value);
-    let testPass = passRegExp.test(getID('password').value);
-    if (testName && testLastN && testEmail && testPass && event.target.checked) {
+//button activation
+getID('form').oninput = function () {
+    const fN = getID('firstName');
+    const lN = getID('lastName');
+    const em = getID('email');
+    const pass = getID('password');
+    const ch = getID('check');
+    if (fN.validity.valid && lN.validity.valid && em.validity.valid &&
+        pass.validity.valid && ch.validity.valid) {
         getID('signUp').disabled = false;
     } else {
         getID('signUp').disabled = true;
     }
 }
-// modal window
+// Submit form. button "signUp"
 getID('form').onsubmit = function () {
     event.preventDefault();
     document.querySelector('.conteiner2').classList.remove('hide');
     document.querySelector('.conteiner1').classList.add('hide');
 }
-
-//  input field validation
-// let form = getID('form')
-// for (const n of form) {
-//     n.addEventListener('input', function () {
-//         let testName = firstNameRegExp.test(getID('firstName').value);
-//         let testLastN = lastNameRegExp.test(getID('lastName').value);
-//         let testEmail = emailRegExp.test(getID('email').value);
-//         let testPass = passRegExp.test(getID('password').value);
-//         if (testName) {
-//             getID('firstName').style.boxShadow = '0 0 5px 5px green';
-//         }
-//         if (testLastN) {
-//             getID('lastName').style.boxShadow = '0 0 5px 5px green';
-//         }
-//         if (testEmail) {
-//             getID('email').style.boxShadow = '0 0 5px 5px green';
-//         }
-//         if (testPass) {
-//             getID('password').style.boxShadow = '0 0 5px 5px green';
-//         }
-//         else {
-//             n.style.boxShadow = '0 0 5px 5px red';
-//         }
-//     })
-// }
